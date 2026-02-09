8e4bee7c597173ece4dec7a00296b8470c9c050c
Bitrequest <47079460+bitrequest@users.noreply.github.com>
2026-02-09 14:16:09 +0100

    Handle multiple connections

--- lightningws.js
+++ lightningws.js
@@ -1,46 +1,41 @@
 const WebSocket = require("ws").Server,
-    express = require("express"),
-    app = express(),
-    server = new WebSocket({
-        "server": app.listen(8040)
-    });
-let object_id = null;
-let subscriptions = [];
-let ws = null;
+	express = require("express"),
+	app = express(),
+	server = new WebSocket({
+		"server": app.listen(8040)
+	});
+
+const clients = new Map();
 
 server.on("connection", socket => {
-    ws = socket;
-    socket.on("message", message => {
-        var object = JSON.parse(message);
-        object_id = object.id;
-        subscriptions.push(object_id);
-        socket.send(JSON.stringify({
-            "pid": object.id,
-            "status": "opened"
-        }));
-    });
-    socket.onclose = function(e) {
-        if (object_id) {
-            var newarr = unsub(subscriptions, object_id);
-            subscriptions = newarr;
-        }
-    };
+	let socket_id = null;
+
+	socket.on("message", message => {
+		const object = JSON.parse(message);
+		socket_id = object.id;
+		clients.set(socket_id, socket);
+		socket.send(JSON.stringify({
+			"pid": object.id,
+			"status": "opened"
+		}));
+	});
+
+	socket.onclose = function() {
+		if (socket_id) {
+			clients.delete(socket_id);
+		}
+	};
 });
 
 app.post("/", function(req, res) {
-    var post_string = req.headers.post,
-        post_object = JSON.parse(post_string),
-        inv_id = post_object.pid;
-    if (ws) {
-        if (subscriptions.includes(inv_id)) {
-            ws.send(post_string);
-        } else {
-            ws.send("no match");
-        }
-    }
-    res.sendStatus(200);
-});
+	const post_string = req.headers.post,
+		post_object = JSON.parse(post_string),
+		inv_id = post_object.pid,
+		client = clients.get(inv_id);
 
-function unsub(subs, pid) {
-    return subs.filter(f => f !== pid);
-}
+	if (client && client.readyState === 1) {
+		client.send(post_string);
+	}
+
+	res.sendStatus(200);
+});
